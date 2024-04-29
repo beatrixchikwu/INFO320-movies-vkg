3297d45ecfbe186f2abf85694025d5159816d9ec
BEATRIX <bwu002@uib.no>
2024-04-29 19:19:49 +0200

    Byttet font, farge og andre småting.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import QueryInput from './QueryInput';
 import QueryResult from './QueryResult';
 import './styles.css';
@@ -7,6 +7,10 @@ import { handleQueryOscarAward, handleQueryDialogueEditor } from './QueryFunctio
 function App() {
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    document.title = "Movies VKG"; 
+  }, []);
 
   const executeQuery = async (query) => {
     try {
@@ -32,8 +36,8 @@ function App() {
   return (
     <div className='container'>
       <div className='content'>
-        <h1 className='header'>Explore our Movie Knowledge Graph</h1>
-        <p className='description-paragraph'>Try some of these suggested queries or enter your own in the box below</p>
+        <h1 className='header'>explore our movie knowledge graph</h1>
+        <p className='description-paragraph'>Try some of these suggested SPARQL queries or enter your own in the box below.</p>
         <div className='ready-made-queries'>
           <button className='ready-made-query-button' onClick={() => handleQueryOscarAward(executeQuery)}>
           Which movies have won an Oscar Award?
@@ -42,7 +46,7 @@ function App() {
           Give me a list of movies and their dialogue editors.
           </button>
           <button className='ready-made-query-button'>
-          Ingunn, legg til flereknapper med de kulere queriesene du lagde.
+          Ingunn, legg til flere knapper med de kulere queriesene du lagde.
           </button>
           <button className='ready-made-query-button'>Test 1</button>
           <button className='ready-made-query-button'>Test 2</button>
